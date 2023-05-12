6e587ba98ed47efc3eee22adb406ab4e10907c65
kxpotenciano <86062808+kxpotenciano@users.noreply.github.com>
2023-05-12 17:21:36 +0800

    update 5/12/2023

--- src/pages/pokemonsearch.js
+++ src/pages/pokemonsearch.js
@@ -121,7 +121,7 @@ export default function PokemonSearch(){
                 return "dark:bg-green-200 text-green-600"
             }
             if (pokemon.type1 === "electric"){
-                return "dark:bg-yellow-200"
+                return "dark:bg-yellow-200 text-yellow-600"
             }
             if (pokemon.type1 === "bug"){
                 return "dark:bg-green-400"
@@ -139,7 +139,7 @@ export default function PokemonSearch(){
                 return "dark:bg-purple-400"
             }
             if (pokemon.type1 === "ground"){
-                return "dark:bg-yellow-800"
+                return "dark:bg-yellow-800 text-white"
             }
             if (pokemon.type1 === "normal"){
                 return "dark:bg-gray-200"
@@ -178,7 +178,7 @@ export default function PokemonSearch(){
                 return "dark:bg-green-200"
             }
             if (pokemon.type2 === "electric"){
-                return "dark:bg-yellow-200"
+                return "dark:bg-yellow-200 text-yellow-600"
             }
             if (pokemon.type2 === "bug"){
                 return "dark:bg-green-400"
@@ -196,7 +196,7 @@ export default function PokemonSearch(){
                 return "dark:bg-purple-400"
             }
             if (pokemon.type2 === "ground"){
-                return "dark:bg-yellow-800"
+                return "dark:bg-yellow-800 text-white"
             }
             if (pokemon.type2 === "normal"){
                 return "dark:bg-gray-200"
@@ -220,7 +220,237 @@ export default function PokemonSearch(){
                 return "dark:bg-pink-400"
             }
         }
+
+        const pokemonMovesColor1 = (moves1) => {
+            if (moves1.type === "fire"){
+                return "dark:bg-red-200"
+            }
+            if (moves1.type === "water"){
+                return "dark:bg-blue-400"
+            }
+            if (moves1.type === "ice"){
+                return "dark:bg-blue-200"
+            }
+            if (moves1.type === "grass"){
+                return "dark:bg-green-200"
+            }
+            if (moves1.type === "electric"){
+                return "dark:bg-yellow-200 text-yellow-600"
+            }
+            if (moves1.type === "bug"){
+                return "dark:bg-green-400"
+            }
+            if (moves1.type === "flying"){
+                return "dark:bg-purple-200"
+            }
+            if (moves1.type === "ghost"){
+                return "dark:bg-purple-800"
+            }
+            if (moves1.type === "dark"){
+                return "dark:bg-black-200"
+            }
+            if (moves1.type === "dragon"){
+                return "dark:bg-purple-400"
+            }
+            if (moves1.type === "ground"){
+                return "dark:bg-yellow-800 text-white"
+            }
+            if (moves1.type === "normal"){
+                return "dark:bg-gray-200"
+            }
+            if (moves1.type === "poison"){
+                return "dark:bg-purple-600"
+            }
+            if (moves1.type === "steel"){
+                return "dark:bg-gray-400"
+            }
+            if (moves1.type === "rock"){
+                return "dark:bg-yellow-600"
+            }
+            if (moves1.type === "fairy"){
+                return "dark:bg-pink-200"
+            }
+            if (moves1.type === "fighting"){
+                return "dark:bg-red-400"
+            }
+            if (moves1.type === "psychic"){
+                return "dark:bg-pink-400"
+            }
+        }
         
+        const pokemonMovesColor2 = (moves2) => {
+            if (moves2.type === "fire"){
+                return "dark:bg-red-200"
+            }
+            if (moves2.type === "water"){
+                return "dark:bg-blue-400"
+            }
+            if (moves2.type === "ice"){
+                return "dark:bg-blue-200"
+            }
+            if (moves2.type === "grass"){
+                return "dark:bg-green-200"
+            }
+            if (moves2.type === "electric"){
+                return "dark:bg-yellow-200 text-yellow-600"
+            }
+            if (moves2.type === "bug"){
+                return "dark:bg-green-400"
+            }
+            if (moves2.type === "flying"){
+                return "dark:bg-purple-200"
+            }
+            if (moves2.type === "ghost"){
+                return "dark:bg-purple-800"
+            }
+            if (moves2.type === "dark"){
+                return "dark:bg-black-200"
+            }
+            if (moves2.type === "dragon"){
+                return "dark:bg-purple-400"
+            }
+            if (moves2.type === "ground"){
+                return "dark:bg-yellow-800 text-white"
+            }
+            if (moves2.type === "normal"){
+                return "dark:bg-gray-200"
+            }
+            if (moves2.type === "poison"){
+                return "dark:bg-purple-600"
+            }
+            if (moves2.type === "steel"){
+                return "dark:bg-gray-400"
+            }
+            if (moves2.type === "rock"){
+                return "dark:bg-yellow-600"
+            }
+            if (moves2.type === "fairy"){
+                return "dark:bg-pink-200"
+            }
+            if (moves2.type === "fighting"){
+                return "dark:bg-red-400"
+            }
+            if (moves2.type === "psychic"){
+                return "dark:bg-pink-400"
+            }
+        }
+
+        const pokemonMovesColor3 = (moves3) => {
+            if (moves3.type === "fire"){
+                return "dark:bg-red-200"
+            }
+            if (moves3.type === "water"){
+                return "dark:bg-blue-400"
+            }
+            if (moves3.type === "ice"){
+                return "dark:bg-blue-200"
+            }
+            if (moves3.type === "grass"){
+                return "dark:bg-green-200"
+            }
+            if (moves3.type === "electric"){
+                return "dark:bg-yellow-200 text-yellow-600"
+            }
+            if (moves3.type === "bug"){
+                return "dark:bg-green-400"
+            }
+            if (moves3.type === "flying"){
+                return "dark:bg-purple-200"
+            }
+            if (moves3.type === "ghost"){
+                return "dark:bg-purple-800"
+            }
+            if (moves3.type === "dark"){
+                return "dark:bg-black-200"
+            }
+            if (moves3.type === "dragon"){
+                return "dark:bg-purple-400"
+            }
+            if (moves3.type === "ground"){
+                return "dark:bg-yellow-800 text-white"
+            }
+            if (moves3.type === "normal"){
+                return "dark:bg-gray-200"
+            }
+            if (moves3.type === "poison"){
+                return "dark:bg-purple-600"
+            }
+            if (moves3.type === "steel"){
+                return "dark:bg-gray-400"
+            }
+            if (moves3.type === "rock"){
+                return "dark:bg-yellow-600"
+            }
+            if (moves3.type === "fairy"){
+                return "dark:bg-pink-200"
+            }
+            if (moves3.type === "fighting"){
+                return "dark:bg-red-400"
+            }
+            if (moves3.type === "psychic"){
+                return "dark:bg-pink-400"
+            }
+        }
+
+        const pokemonMovesColor4 = (moves4) => {
+            if (moves4.type === "fire"){
+                return "dark:bg-red-200"
+            }
+            if (moves4.type === "water"){
+                return "dark:bg-blue-400"
+            }
+            if (moves4.type === "ice"){
+                return "dark:bg-blue-200"
+            }
+            if (moves4.type === "grass"){
+                return "dark:bg-green-200"
+            }
+            if (moves4.type === "electric"){
+                return "dark:bg-yellow-200 text-yellow-600"
+            }
+            if (moves4.type === "bug"){
+                return "dark:bg-green-400"
+            }
+            if (moves4.type === "flying"){
+                return "dark:bg-purple-200"
+            }
+            if (moves4.type === "ghost"){
+                return "dark:bg-purple-800"
+            }
+            if (moves4.type === "dark"){
+                return "dark:bg-black-200"
+            }
+            if (moves4.type === "dragon"){
+                return "dark:bg-purple-400"
+            }
+            if (moves4.type === "ground"){
+                return "dark:bg-yellow-800 text-white"
+            }
+            if (moves4.type === "normal"){
+                return "dark:bg-gray-200"
+            }
+            if (moves4.type === "poison"){
+                return "dark:bg-purple-600"
+            }
+            if (moves4.type === "steel"){
+                return "dark:bg-gray-400"
+            }
+            if (moves4.type === "rock"){
+                return "dark:bg-yellow-600"
+            }
+            if (moves4.type === "fairy"){
+                return "dark:bg-pink-200"
+            }
+            if (moves4.type === "fighting"){
+                return "dark:bg-red-400"
+            }
+            if (moves4.type === "psychic"){
+                return "dark:bg-pink-400"
+            }
+        }
+
+        
+
     return(
         <>
         <Header/>
@@ -239,18 +469,18 @@ export default function PokemonSearch(){
         <div className="inline-block">
             {!chosenPokemon ? (<h1 className="text-white text-2xl">Choose a Pokemon</h1>) : (
                 <>
-                <div class="inline-block bg-white border border-pink-200 rounded-lg shadow dark:bg-pink-300 dark:border-white justify-center w-[450px] opacity-90">
+                <div class="inline-block bg-white border border-pink-200 rounded-lg shadow dark:bg-pink-300 dark:border-white justify-center w-[450px] opacity-90"> <br/>
                     <img src={pokemon.img} className="inline-block h-[150px] w-[150px] opacity-100"/>
                 <div class="p-5">
                         <p className="mb-3 font-bold text-pink-700 dark:text-pink-600 text-lg">{pokemon.name}</p>
                         <p className="mb-3 font-normal text-pink-700 dark:text-pink-600 text-lg">Type: <div className={`inline-block border border-pink-200 rounded-lg shadow ${pokemonTypeColor1(pokemon)} dark:border-white justify-center w-[100px] text-white`} >{pokemon.type1}</div> <div className={`inline-block border border-pink-200 rounded-lg shadow ${pokemonTypeColor2(pokemon)} dark:border-white justify-center w-[100px] text-white`}>{pokemon.type2}</div></p>
-                        <p className="mb-3 font-normal text-pink-700 dark:text-pink-600 text-lg">Move 1: {pokemon.move1} [Type: {moves1.type} PP: {moves1.pp}/{moves1.pp}]</p>
-                        <p className="mb-3 font-normal text-pink-700 dark:text-pink-600 text-lg">Move 2: {pokemon.move2} [Type: {moves2.type} PP: {moves2.pp}/{moves2.pp}]</p>
-                        <p className="mb-3 font-normal text-pink-700 dark:text-pink-600 text-lg">Move 3: {pokemon.move3} [Type: {moves3.type} PP: {moves3.pp}/{moves3.pp}]</p>
-                        <p className="mb-3 font-normal text-pink-700 dark:text-pink-600 text-lg">Move 4: {pokemon.move4} [Type: {moves4.type} PP: {moves4.pp}/{moves4.pp}]</p>
-                        <p className="mb-3 font-normal text-pink-700 dark:text-pink-600 text-lg">HP: {pokemon.hp} | Speed: {pokemon.speed}</p>
-                        <p className="mb-3 font-normal text-pink-700 dark:text-pink-600 text-lg">Attack: {pokemon.attack} | Special Attack: {pokemon.special_attack}</p>
-                        <p className="mb-3 font-normal text-pink-700 dark:text-pink-600 text-lg">Defense: {pokemon.defense} | Special Defense: {pokemon.special_defense}</p>
+                        <p className="mb-3 font-normal text-pink-700 dark:text-pink-600 text-lg"><div className={`inline-block border border-pink-200 rounded-lg shadow dark:bg-pink-200 dark:border-white justify-center w-[300px] text-gray-700`}><div className={`inline-block float-left border border-pink-200 rounded-lg shadow ${pokemonMovesColor1(moves1)} dark:border-gray-500 justify-center w-[100px] text-black-700`}>{moves1.type}</div> {pokemon.move1}   <div className="inline-block text-sm float-right">PP: {moves1.pp}/{moves1.pp}</div></div></p>
+                        <p className="mb-3 font-normal text-pink-700 dark:text-pink-600 text-lg"><div className={`inline-block border border-pink-200 rounded-lg shadow dark:bg-pink-200 dark:border-white justify-center w-[300px] text-gray-700`}><div className={`inline-block float-left border border-pink-200 rounded-lg shadow ${pokemonMovesColor2(moves2)} dark:border-gray-500 justify-center w-[100px] text-black-700`}>{moves2.type}</div> {pokemon.move2}   <div className="inline-block text-sm float-right">PP: {moves2.pp}/{moves2.pp}</div></div></p>
+                        <p className="mb-3 font-normal text-pink-700 dark:text-pink-600 text-lg"><div className={`inline-block border border-pink-200 rounded-lg shadow dark:bg-pink-200 dark:border-white justify-center w-[300px] text-gray-700`}><div className={`inline-block float-left border border-pink-200 rounded-lg shadow ${pokemonMovesColor3(moves3)} dark:border-gray-500 justify-center w-[100px] text-black-700`}>{moves3.type}</div> {pokemon.move3}   <div className="inline-block text-sm float-right">PP: {moves3.pp}/{moves3.pp}</div></div></p>
+                        <p className="mb-3 font-normal text-pink-700 dark:text-pink-600 text-lg"><div className={`inline-block border border-pink-200 rounded-lg shadow dark:bg-pink-200 dark:border-white justify-center w-[300px] text-gray-700`}><div className={`inline-block float-left border border-pink-200 rounded-lg shadow ${pokemonMovesColor4(moves4)} dark:border-gray-500 justify-center w-[100px] text-black-700`}>{moves4.type}</div> {pokemon.move4}   <div className="inline-block text-sm float-right">PP: {moves4.pp}/{moves4.pp}</div></div></p>
+                        <p className="mb-3 font-bold text-pink-700 dark:text-pink-600 text-lg">HP: <div className="inline-block text-white font-normal">{pokemon.hp}</div> | Speed: <div className="inline-block text-white font-normal">{pokemon.speed}</div></p>
+                        <p className="mb-3 font-bold text-pink-700 dark:text-pink-600 text-lg">Attack: <div className="inline-block text-white font-normal">{pokemon.attack}</div> | Special Attack:<div className="inline-block text-white font-normal">{pokemon.special_attack}</div></p>
+                        <p className="mb-3 font-bold text-pink-700 dark:text-pink-600 text-lg">Defense: <div className="inline-block text-white font-normal">{pokemon.defense}</div> | Special Defense: <div className="inline-block text-white font-normal">{pokemon.special_defense}</div></p>
                     </div>
                 </div>
                 </>
